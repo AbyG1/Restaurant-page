16efba072200563081c0f57483ce65cf6b3e5e99
Aby <aby@zoondia.net>
2026-01-19 13:32:32 +0530

    Add tab browsing logic

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@ import homePage from "./home.js"
 import menuPage from "./menu.js"
 import contactPage from "./contact.js";
 
+const container = document.getElementById("content");
 
+
+document.addEventListener('DOMContentLoaded', function() {
+        container.innerHTML = ''
+        container.appendChild(homePage());
+});
+
+
+
+document.addEventListener("click",(e) => {
+
+    if((e.target.id) === "homeBtn"){
+        container.innerHTML = ''
+        container.appendChild(homePage());
+    }
+
+    if((e.target.id) === "menuBtn"){
+        container.innerHTML = ''
+        container.appendChild(menuPage());
+    }
+
+     if((e.target.id) === "contactBtn"){
+        container.innerHTML = ''
+        container.appendChild(contactPage());
+    }
+
+
+})
